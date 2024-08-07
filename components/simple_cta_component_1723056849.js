9284d04be8a0dbf0df08fe57fa8e54b20eba6cb9
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-08-07 20:04:39 +0000

    Code generation

--- components/simple_cta_component_1723056849.js
+++ components/simple_cta_component_1723056849.js
@@ -15,6 +15,7 @@ Vue.component("simple_cta_component_1723056849", {
                 
                 <div id="cta-button-container" class="flex">
                     <a id="cta-button" href="#" class="flex-1 text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800">Start Your Fitness Journey</a>
+                    <button class="flex-1 text-purple-700 bg-white hover:bg-gray-100 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 border border-purple-700">Free</button>
                 </div>
             </div>
         </div>
